b6029189d592114dd2a5b7cb6fbe5b94b0bb7c43
jirviato29 <jirviato29@gmail.com>
2026-06-08 12:58:55 -0600

    chore: ignorar carpeta ATO-App-master (repo muerto, no se despliega)

--- ATO-App-master/migrations/add_comision_pagada_to_venta_chips.sql
+++
@@ -1,6 +0,0 @@
--- Migración: agrega columna comision_pagada a venta_chips
--- Fecha: 2026-04-27
--- Ejecutar una sola vez en producción
-
-ALTER TABLE venta_chips
-    ADD COLUMN IF NOT EXISTS comision_pagada BOOLEAN NOT NULL DEFAULT FALSE;
